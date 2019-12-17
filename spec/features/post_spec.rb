6ba799b4b0ced712ebda62d080271e1b6a3ddf04
majovainilla <maria_reyes@me.com>
2019-12-17 09:38:40 -0600

    Style user views

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -4,9 +4,9 @@ require 'rails_helper'
 
 RSpec.feature 'Posts', type: :feature do
   scenario 'A user posts successfully' do
-    user = User.create(username: 'John',
-                       email: 'john@example.com',
-                       password: 'password')
+    User.create(username: 'John',
+                email: 'john@example.com',
+                password: 'password')
     visit login_path
     page.fill_in 'user_email', with: 'john@example.com'
     page.fill_in 'user_password', with: 'password'
