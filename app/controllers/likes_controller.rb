51f1ad0dc289252b0afc698e68964b04072b9aac
Fabricio Garcia <gael.fabricio@gmail.com>
2019-12-17 15:36:08 -0600

    create likes view and controllers

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,18 @@
 class LikesController < ApplicationController
+
+  def new
+    @like = Like.new
+  end
+
+  def create
+    @like = Like.new(like_params)
+    @like.user = current_user
+    redirect_to root_path if @like.save
+  end
+
+  private
+
+  def like_params
+    params.require(:like).permit(:post_id)
+  end
 end
