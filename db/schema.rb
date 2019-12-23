3931611dffc5a91e5e58526a897e3df7b9aa716d
Fabricio Garcia <gael.fabricio@gmail.com>
2019-12-23 14:29:19 -0600

    create migration to delete  extra columns in user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_23_155216) do
+ActiveRecord::Schema.define(version: 2019_12_23_202311) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,13 +61,9 @@ ActiveRecord::Schema.define(version: 2019_12_23_155216) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "username"
-    t.bigint "post_id"
     t.string "provider"
     t.string "uid"
-    t.string "name"
-    t.text "image"
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["post_id"], name: "index_users_on_post_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
@@ -76,5 +72,4 @@ ActiveRecord::Schema.define(version: 2019_12_23_155216) do
   add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users"
   add_foreign_key "posts", "users"
-  add_foreign_key "users", "posts"
 end
