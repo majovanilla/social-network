9e1b629766474e521d03bce5df0e82641f6f2a0a
Fabricio Garcia <gael.fabricio@gmail.com>
2019-12-23 14:17:08 -0600

    modify settings for heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -47,8 +47,7 @@ class User < ApplicationRecord
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
-      user.name = auth.info.name   # assuming the user model has a name
-      user.image = auth.info.image # assuming the user model has an image
+      user.username = auth.info.name
     end
   end
 end
