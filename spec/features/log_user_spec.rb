879bd5d43fe3059fb35da2fc6cf79e9c4eb50966
majovainilla <maria_reyes@me.com>
2019-12-23 15:58:14 -0600

    Add feature test

--- spec/features/log_user_spec.rb
+++ spec/features/log_user_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 
 RSpec.feature 'Log in and out', type: :feature do
   scenario 'Succesfully logs in/out an existing user' do
-    user = User.create(username: 'John',
+    User.create(username: 'John',
                        email: 'john@example.com',
                        password: 'password')
     visit login_path
@@ -12,11 +12,11 @@ RSpec.feature 'Log in and out', type: :feature do
     page.fill_in 'user_password', with: 'password'
     click_on(class: 'login')
     expect(page.current_path).to eq root_path
-    #have_link user.username, href: user_path(user)
     have_link 'Users', href: users_path
-    have_link 'Log out', href: logout_path
+    have_link 'Log out', href: '/users/sign_out'
     have_content 'posts-list'
     click_on 'Log out'
-    expect(page.current_path).to eq user_session_path
+    expect(page.current_path).to eq new_user_session_path
+    have_content 'Log in'
   end
 end
