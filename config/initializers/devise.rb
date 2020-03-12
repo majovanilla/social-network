9a7d3501047bb97cf32ccf0ca5f0f8416e59fb88
majovainilla <maria_reyes@me.com>
2020-03-12 11:02:38 -0600

    Changed api keys to env variables

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -259,7 +259,7 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  config.omniauth :facebook, 'APP_ID', 'APP_SECRET', token_params: { parse: :json }
+  config.omniauth :facebook, ENV['APP_ID'], ENV['APP_SECRET'], token_params: { parse: :json }
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
